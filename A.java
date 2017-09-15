52d65ebd58eaddf48bb6273da1e368a153dd42c3
Aditya Nawal <learning@DIN86000534.corp.capgemini.com>
2017-09-15 18:04:13 +0530

    i lovepass

--- A.java
+++ A.java
@@ -2,6 +2,8 @@ class A{
 public static void main(String[] args){
 System.out.println("Single");
 System.out.println("Devone");
+B as= new B();
+as.getData();
 
 }
 
---
+++ B.java
@@ -0,0 +1,5 @@
+class B{
+public getData(){
+System.out.println("Hello");
+}
+}
